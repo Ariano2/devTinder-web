e91c43eefcd6bcf5f11f0ff000e458d75c433b9f
Ariano2 <aryanramjasian@gmail.com>
2024-12-24 18:18:45 +0530

    Add routing and setup a Components folder
    Using the react router library we have added multiple routes and created certain basic components to render

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { BrowserRouter, Routes, Route } from 'react-router';
+import App from './App.jsx';
+import Login from './components/Login.jsx';
+import Profile from './components/Profile.jsx';
+import Connections from './components/Connections.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="login" element={<Login />}></Route>
+        <Route path="connections" element={<Connections />}></Route>
+        <Route path="profile" element={<Profile />}></Route>
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
