69cef53f9a48a8f3996f4b62d2e0fc96c80a9367
Ariano2 <aryanramjasian@gmail.com>
2025-01-09 19:13:38 +0530

    Create Profile View and Edit Section

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,5 +1,141 @@
+import { useDispatch, useSelector } from 'react-redux';
+import UserCard from './UserCard';
+import { useState } from 'react';
+import { BASE_URL } from './../utils/constants';
+import axios from 'axios';
+import { addUser } from '../utils/userSlice';
 const Profile = () => {
-  return <div>Profile</div>;
+  const user = useSelector((store) => store.user);
+  const [error, setError] = useState();
+  const [firstName, setFirstName] = useState(user.firstName);
+  const [lastName, setLastName] = useState(user.lastName);
+  const [age, setAge] = useState(user?.age || 'age not set');
+  const [photoUrl, setPhotoUrl] = useState(user.photoUrl);
+  const [gender, setGender] = useState(user?.gender || 'unspecified gender');
+  const [about, setAbout] = useState(user?.about || 'This is default About');
+  const [updateSuccess, setUpdateSuccess] = useState(false);
+  const dispatch = useDispatch();
+  const saveProfile = async () => {
+    setError();
+    try {
+      const res = await axios.patch(
+        BASE_URL + '/profile/edit',
+        { firstName, lastName, age, photoUrl, gender, about },
+        { withCredentials: true }
+      );
+      dispatch(addUser(res.data.loggedInUser));
+      setUpdateSuccess(true);
+      setTimeout(() => {
+        setUpdateSuccess(false);
+      }, 3000);
+    } catch (err) {
+      setError(err.response.data);
+    }
+  };
+  return (
+    user && (
+      <>
+        {updateSuccess && (
+          <div className="toast toast-top toast-center">
+            <div className="alert alert-success">
+              <span>Profile Updated Successfully</span>
+            </div>
+          </div>
+        )}
+        <div className="flex gap-10 px-[20%] py-10">
+          <div className="card-body items-center text-center bg-base-200">
+            <h2 className="card-title">Update Profile</h2>
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">firstName</span>
+              </div>
+              <input
+                type="text"
+                className="input input-bordered w-full max-w-xs"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+              />
+            </label>
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">lastName</span>
+              </div>
+              <input
+                type="text"
+                className="input input-bordered w-full max-w-xs"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+              />
+            </label>
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">age</span>
+              </div>
+              <input
+                type="number"
+                className="input input-bordered w-full max-w-xs"
+                value={age}
+                onChange={(e) => setAge(e.target.value)}
+              />
+            </label>
+            <select
+              defaultValue={user.gender}
+              value={gender}
+              onChange={(e) => {
+                setGender(e.target.value);
+              }}
+              className="select select-primary mt-4 w-full max-w-xs"
+            >
+              <option value="select" disabled>
+                Gender
+              </option>
+              <option value="male">male</option>
+              <option value="female">female</option>
+              <option value="other">other</option>
+            </select>
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">photoUrl</span>
+              </div>
+              <input
+                type="url"
+                className="input input-bordered w-full max-w-xs"
+                value={photoUrl}
+                onChange={(e) => setPhotoUrl(e.target.value)}
+              />
+            </label>
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">About</span>
+              </div>
+              <textarea
+                className="textarea textarea-bordered h-24 "
+                value={about}
+                onChange={(e) => {
+                  setAbout(e.target.value);
+                }}
+              ></textarea>
+            </label>
+            {error && <p className="text-error">{error}</p>}
+            <div className="card-actions justify-end my-4">
+              <button
+                type="submit"
+                onClick={saveProfile}
+                className="btn btn-primary"
+              >
+                Save Profile
+              </button>
+            </div>
+          </div>
+          <div>
+            <UserCard
+              user={{ firstName, lastName, age, photoUrl, gender, about }}
+            />
+          </div>
+        </div>
+      </>
+    )
+  );
 };
 
 export default Profile;
--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -3,12 +3,14 @@ const UserCard = ({ user }) => {
   return (
     <div className="card bg-base-300 w-96 shadow-xl">
       <figure>
-        <img src={photoUrl} alt="Shoes" />
+        <img src={photoUrl} alt="Please provide a valid photoUrl" />
       </figure>
       <div className="card-body">
         <h2 className="card-title">{firstName + ' ' + lastName}</h2>
-        <p>{age + ', ' + gender.toUpperCase()}</p>
-        <p>{about}</p>
+        {age && gender && (
+          <p>{age + ', ' + gender[0].toUpperCase() + gender.slice(1)}</p>
+        )}
+        {about && <p>{about}</p>}
         <div className="card-actions my-6 justify-around flex font-semibold">
           <button className="btn btn-secondary">Ignore</button>
           <button className="btn btn-success">Interested</button>
